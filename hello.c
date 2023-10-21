8d0eeeb51b519faf8ac5f984eb6320b0f2548cda
shoaib sakhawat <shoaibkarlal23@gmail.com>
2023-10-21 17:28:00 +0500

    i love the school of computing

--- hello.c
+++ hello.c
@@ -1,7 +1,7 @@
 /**
- * Author: NAME
- *         EMAIL
- * Date: 20xx/xx/xx
+ * Author: shoaib sakhawat
+ *         EMAIL shoaibkarlal23@gmail.com  
+ * Date: 20/10/23
  *
  * A simple hello world program in C
  *
@@ -11,7 +11,7 @@
 
 int main(int argc, char **argv) {
 
-  printf("Hello World!\n");
+  printf("i love the school of computing!\n");
 
   return 0;
 }
